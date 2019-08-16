e705a6a80465c15c8d817bc87f984bddd7069e8a
subarna <khadka.subarnabijaya@gmail.com>
2019-08-16 15:07:43 -0400

    Added error handling !!

--- COMP123-S2019-A5-300988937/Views/ProductInfoForm.cs
+++ COMP123-S2019-A5-300988937/Views/ProductInfoForm.cs
@@ -158,44 +158,66 @@ namespace COMP123_S2019_A5_300988937.Views
             if (result != DialogResult.Cancel)
             {
                 //open filestream 
-                using (BinaryReader inputStream = new BinaryReader(File.Open(ProductOpenFileDialog.FileName, FileMode.Open)))
+                try
                 {
-                    //read from file into product object
-                    Program.product.productID = short.Parse(inputStream.ReadString());
-                    Program.product.cost = decimal.Parse(inputStream.ReadString());
-                    Program.product.manufacturer = inputStream.ReadString();
-                    Program.product.model = inputStream.ReadString();
-                    Program.product.RAM_type = inputStream.ReadString();
-                    Program.product.RAM_size = inputStream.ReadString();
-                    Program.product.displaytype = inputStream.ReadString();
-                    Program.product.screensize = inputStream.ReadString();
-                    Program.product.resolution = inputStream.ReadString();
-                    Program.product.CPU_Class = inputStream.ReadString();
-                    Program.product.CPU_brand = inputStream.ReadString();
-                    Program.product.CPU_type = inputStream.ReadString();
-                    Program.product.CPU_speed = inputStream.ReadString();
-                    Program.product.CPU_number = inputStream.ReadString();
-                    Program.product.condition = inputStream.ReadString();
-                    Program.product.OS = inputStream.ReadString();
-                    Program.product.platform = inputStream.ReadString();
-                    Program.product.HDD_size = inputStream.ReadString();
-                    Program.product.HDD_speed = inputStream.ReadString();
-                    Program.product.GPU_Type = inputStream.ReadString();
-                    Program.product.optical_drive = inputStream.ReadString();
-                    Program.product.Audio_type = inputStream.ReadString();
-                    Program.product.LAN = inputStream.ReadString();
-                    Program.product.WIFI = inputStream.ReadString();
-                    Program.product.width = inputStream.ReadString();
-                    Program.product.height = inputStream.ReadString();
-                    Program.product.depth = inputStream.ReadString();
-                    Program.product.weight = inputStream.ReadString();
-                    Program.product.moust_type = inputStream.ReadString();
-                    Program.product.power = inputStream.ReadString();
-                    Program.product.webcam = inputStream.ReadString();
+                    using (BinaryReader inputStream = new BinaryReader(File.Open(ProductOpenFileDialog.FileName, FileMode.Open)))
+                    {
+                        //read from file into product object
+                        try
+                        {
+                            Program.product.productID = short.Parse(inputStream.ReadString());
+                            Program.product.cost = decimal.Parse(inputStream.ReadString());
+                            Program.product.manufacturer = inputStream.ReadString();
+                            Program.product.model = inputStream.ReadString();
+                            Program.product.RAM_type = inputStream.ReadString();
+                            Program.product.RAM_size = inputStream.ReadString();
+                            Program.product.displaytype = inputStream.ReadString();
+                            Program.product.screensize = inputStream.ReadString();
+                            Program.product.resolution = inputStream.ReadString();
+                            Program.product.CPU_Class = inputStream.ReadString();
+                            Program.product.CPU_brand = inputStream.ReadString();
+                            Program.product.CPU_type = inputStream.ReadString();
+                            Program.product.CPU_speed = inputStream.ReadString();
+                            Program.product.CPU_number = inputStream.ReadString();
+                            Program.product.condition = inputStream.ReadString();
+                            Program.product.OS = inputStream.ReadString();
+                            Program.product.platform = inputStream.ReadString();
+                            Program.product.HDD_size = inputStream.ReadString();
+                            Program.product.HDD_speed = inputStream.ReadString();
+                            Program.product.GPU_Type = inputStream.ReadString();
+                            Program.product.optical_drive = inputStream.ReadString();
+                            Program.product.Audio_type = inputStream.ReadString();
+                            Program.product.LAN = inputStream.ReadString();
+                            Program.product.WIFI = inputStream.ReadString();
+                            Program.product.width = inputStream.ReadString();
+                            Program.product.height = inputStream.ReadString();
+                            Program.product.depth = inputStream.ReadString();
+                            Program.product.weight = inputStream.ReadString();
+                            Program.product.moust_type = inputStream.ReadString();
+                            Program.product.power = inputStream.ReadString();
+                            Program.product.webcam = inputStream.ReadString();
+                        }
+                        catch(Exception exp)
+                        {
+                            MessageBox.Show("ERROR: " + exp.Message, "ERROR",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                      
 
-                    //cleanup
-                    inputStream.Close();
-                    inputStream.Dispose();
+                        //cleanup
+                        inputStream.Close();
+                        inputStream.Dispose();
+                    }
+                }
+                catch (IOException ioexception)
+                {
+                    MessageBox.Show("ERROR: " + ioexception.Message, "ERROR",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch(Exception exception)
+                {
+                    MessageBox.Show("ERROR: " + exception.Message, "ERROR",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
                 ProductInfoForm_Activated(sender, e);
             }
--- COMP123-S2019-A5-300988937/Views/StartForm.cs
+++ COMP123-S2019-A5-300988937/Views/StartForm.cs
@@ -20,6 +20,9 @@ namespace COMP123_S2019_A5_300988937.Views
 {
     public partial class StartForm : Form
     {
+        //flag to check if some error has occured
+       // used to switch between startform and product info form
+        private bool _errflag = false;
         public StartForm()
         {
             InitializeComponent();
@@ -41,6 +44,7 @@ namespace COMP123_S2019_A5_300988937.Views
         /// <param name="e"></param>
         private void OpenSavedOrderButton_Click(object sender, EventArgs e)
         {
+            _errflag = false; //this flag should be set to false when user starts to open from file
             //Configuration for openFileDialog
             ProductOpenFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
             ProductOpenFileDialog.FileName = "DefaultProduct.dat";
@@ -54,44 +58,56 @@ namespace COMP123_S2019_A5_300988937.Views
                 using (BinaryReader inputStream = new BinaryReader(File.Open(ProductOpenFileDialog.FileName, FileMode.Open)))
                 {
                     //read from file into product object
-                    Program.product.productID = short.Parse(inputStream.ReadString());
-                    Program.product.cost = decimal.Parse(inputStream.ReadString());
-                    Program.product.manufacturer = inputStream.ReadString();
-                    Program.product.model = inputStream.ReadString();
-                    Program.product.RAM_type = inputStream.ReadString();
-                    Program.product.RAM_size = inputStream.ReadString();
-                    Program.product.displaytype = inputStream.ReadString();
-                    Program.product.screensize = inputStream.ReadString();
-                    Program.product.resolution = inputStream.ReadString();
-                    Program.product.CPU_Class = inputStream.ReadString();
-                    Program.product.CPU_brand = inputStream.ReadString();
-                    Program.product.CPU_type = inputStream.ReadString();
-                    Program.product.CPU_speed = inputStream.ReadString();
-                    Program.product.CPU_number = inputStream.ReadString();
-                    Program.product.condition = inputStream.ReadString();
-                    Program.product.OS = inputStream.ReadString();
-                    Program.product.platform = inputStream.ReadString();
-                    Program.product.HDD_size = inputStream.ReadString();
-                    Program.product.HDD_speed = inputStream.ReadString();
-                    Program.product.GPU_Type = inputStream.ReadString();
-                    Program.product.optical_drive = inputStream.ReadString();
-                    Program.product.Audio_type = inputStream.ReadString();
-                    Program.product.LAN = inputStream.ReadString();
-                    Program.product.WIFI = inputStream.ReadString();
-                    Program.product.width = inputStream.ReadString();
-                    Program.product.height = inputStream.ReadString();
-                    Program.product.depth = inputStream.ReadString();
-                    Program.product.weight = inputStream.ReadString();
-                    Program.product.moust_type = inputStream.ReadString();
-                    Program.product.power = inputStream.ReadString();
-                    Program.product.webcam = inputStream.ReadString();
-
+                    try
+                    {
+                        Program.product.productID = short.Parse(inputStream.ReadString());
+                        Program.product.cost = decimal.Parse(inputStream.ReadString());
+                        Program.product.manufacturer = inputStream.ReadString();
+                        Program.product.model = inputStream.ReadString();
+                        Program.product.RAM_type = inputStream.ReadString();
+                        Program.product.RAM_size = inputStream.ReadString();
+                        Program.product.displaytype = inputStream.ReadString();
+                        Program.product.screensize = inputStream.ReadString();
+                        Program.product.resolution = inputStream.ReadString();
+                        Program.product.CPU_Class = inputStream.ReadString();
+                        Program.product.CPU_brand = inputStream.ReadString();
+                        Program.product.CPU_type = inputStream.ReadString();
+                        Program.product.CPU_speed = inputStream.ReadString();
+                        Program.product.CPU_number = inputStream.ReadString();
+                        Program.product.condition = inputStream.ReadString();
+                        Program.product.OS = inputStream.ReadString();
+                        Program.product.platform = inputStream.ReadString();
+                        Program.product.HDD_size = inputStream.ReadString();
+                        Program.product.HDD_speed = inputStream.ReadString();
+                        Program.product.GPU_Type = inputStream.ReadString();
+                        Program.product.optical_drive = inputStream.ReadString();
+                        Program.product.Audio_type = inputStream.ReadString();
+                        Program.product.LAN = inputStream.ReadString();
+                        Program.product.WIFI = inputStream.ReadString();
+                        Program.product.width = inputStream.ReadString();
+                        Program.product.height = inputStream.ReadString();
+                        Program.product.depth = inputStream.ReadString();
+                        Program.product.weight = inputStream.ReadString();
+                        Program.product.moust_type = inputStream.ReadString();
+                        Program.product.power = inputStream.ReadString();
+                        Program.product.webcam = inputStream.ReadString();
+                    }
+                    catch(Exception exception)
+                    {
+                        _errflag = true; //this flag is set to true when some error is caught
+                        MessageBox.Show("ERROR: " + exception.Message, "ERROR",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
                     //cleanup
                     inputStream.Close();
                     inputStream.Dispose();
                 }
-                Program.Forms[FormName.PRODUCT_INFO_FORM].Show();
-                Hide();
+                //when everything is good, product info form is displayed, else remains at start form
+                if (_errflag == false) {
+                    Program.Forms[FormName.PRODUCT_INFO_FORM].Show();
+                    Hide();
+                }
+                
             }
             
         }
